6d4b2f334b06787ea4f05b54f72b0aacbe5280c1
Carlinhao <carsilva.ti@gmail.com>
2021-03-26 21:59:12 -0300

    Adicionando metodo para esconder teclado.

--- src/main/java/appium/utilities/InsertTextHelper.java
+++ src/main/java/appium/utilities/InsertTextHelper.java
@@ -5,11 +5,16 @@ import java.net.MalformedURLException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
+import appium.base.AppiumStart;
+
 public class InsertTextHelper {
 
 	public static void insertText(WebElement element, String text) throws MalformedURLException {
 		DynamicWaits.waitElement().until(ExpectedConditions.elementToBeClickable(element));
 		element.clear();
 		element.sendKeys(text);
+		AppiumStart
+			.androidDriver()
+			.hideKeyboard();
 	}
 }
