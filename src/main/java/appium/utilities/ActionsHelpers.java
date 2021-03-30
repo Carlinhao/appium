4971caecb8e382306dcd92ad596a63442a0b184f
Carlinhao <carsilva.ti@gmail.com>
2021-03-29 22:39:56 -0300

    Criando metodo de ação tocar em um elemento da tela do app.

--- src/main/java/appium/utilities/ActionsHelpers.java
+++ src/main/java/appium/utilities/ActionsHelpers.java
@@ -1,5 +1,10 @@
 package appium.utilities;
 
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.interactions.touch.TouchActions;
+
+import appium.base.AppiumStart;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 
@@ -7,5 +12,12 @@ public class ActionsHelpers {
 
 	private AndroidDriver<MobileElement> _driver;
 	
+	public ActionsHelpers() throws MalformedURLException {
+		_driver = AppiumStart.androidDriver();
+	}
 	
+	public void tapElement(MobileElement element) {
+		new TouchActions(_driver)
+		.singleTap(element).perform();
+	}
 }
