e28419ab3f95fd7f8c4408017254dab494d54cef
Carlinhao <carsilva.ti@gmail.com>
2021-03-29 22:38:28 -0300

    Criando metodo de ação tocar em um elemento da tela do app.

--- src/main/java/appium/utilities/ActionsHelpers.java
+++ src/main/java/appium/utilities/ActionsHelpers.java
@@ -1,5 +1,11 @@
 package appium.utilities;
 
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
 public class ActionsHelpers {
 
+	private AndroidDriver<MobileElement> _driver;
+	
+	
 }
