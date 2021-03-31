4d00c07c86d4e209514b4a2492e619ff029741b6
Carlinhao <carsilva.ti@gmail.com>
2021-03-31 16:48:25 -0300

    Criando classe para armazenar os elementos de interação do app.

---
+++ src/main/java/appium/utilities/ElementsConfiguration.java
@@ -0,0 +1,24 @@
+package appium.utilities;
+
+public class ElementsConfiguration {
+	
+	// Actions
+	public static final String btn_Point = "dec_point";
+	public static final String btn_Del = "del";
+	public static final String btn_Sum = "op_add";
+	public static final String btn_Div = "op_div";
+	public static final String btn_Sub = "op_sub";
+	public static final String btn_Mult = "op_mul";
+	
+	// Numbers
+	public static final String btnNumber_0 = "digit_0";
+	public static final String btnNumber_1 = "digit_1";
+	public static final String btnNumber_2 = "digit_2";
+	public static final String btnNumber_3 = "digit_3";
+	public static final String btnNumber_4 = "digit_4";
+	public static final String btnNumber_5 = "digit_5";
+	public static final String btnNumber_6 = "digit_6";
+	public static final String btnNumber_7 = "digit_7";
+	public static final String btnNumber_8 = "digit_8";
+	public static final String btnNumber_9 = "digit_9";
+}
