29456ec83b33c53db44d655671cbc1f008157377
Carlinhao <carsilva.ti@gmail.com>
2021-03-31 16:39:20 -0300

    Adicionando propriedades para interagir com elementos da tela

--- src/main/java/appium/pages/CalculatorPageObjects.java
+++ src/main/java/appium/pages/CalculatorPageObjects.java
@@ -1,5 +1,26 @@
 package appium.pages;
 
+import org.openqa.selenium.support.FindBy;
+
+import io.appium.java_client.android.AndroidElement;
+
 public class CalculatorPageObjects {
 
+	@FindBy (id = "calculator_button")
+	AndroidElement btnCalculator;
+	
+	@FindBy (id = "")
+	AndroidElement btnSum;
+	
+	@FindBy (id = "")
+	AndroidElement btnDiv;
+	
+	@FindBy (id = "")
+	AndroidElement btnSub;
+	
+	@FindBy (id = "")
+	AndroidElement btnMilt;
+	
+	@FindBy (id = "")
+	AndroidElement btnPoint;
 }
