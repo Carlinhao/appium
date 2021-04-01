b41d1ba2b34797759c5c23bab4c1ddf886b3d638
Carlinhao <carsilva.ti@gmail.com>
2021-04-01 19:31:45 -0300

    Refatoração de código

--- src/Test/java/appium/tests/LoginTest.java
+++ src/Test/java/appium/tests/LoginTest.java
@@ -1,17 +1,32 @@
 package appium.tests;
 
+import java.net.MalformedURLException;
+
 import org.junit.Test;
 
 import com.aventstack.extentreports.Status;
+
+import appium.pages.SumPage;
 import appium.utilities.ExtentReportConfiguration;
 
 public class LoginTest extends BaseTest {
 
+	private SumPage _sumPatge;
+	
+	public LoginTest() throws MalformedURLException {
+		_sumPatge = new SumPage();
+	}
+	
 	@Test
 	public void startParler() {
+		_sumPatge.getSumResult();		
+		
 		ExtentReportConfiguration.getInstance()
 		.createTest("Login Test")
 		.log(Status.PASS, "Start login test.");
+		
+		
+		
 		
 		ExtentReportConfiguration
 		.getInstance()
