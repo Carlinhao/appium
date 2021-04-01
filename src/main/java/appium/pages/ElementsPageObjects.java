d44c33ebde371d9fd50468efb24ddc363f08bb25
Carlinhao <carsilva.ti@gmail.com>
2021-04-01 20:00:52 -0300

    Adicionando AppiumFildDecorator in page factory, e alterando o tipo dos elementos.

--- src/main/java/appium/pages/ElementsPageObjects.java
+++ src/main/java/appium/pages/ElementsPageObjects.java
@@ -2,67 +2,66 @@ package appium.pages;
 
 import java.net.MalformedURLException;
 
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import appium.base.AppiumStart;
 import appium.utilities.ElementsConfiguration;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 
-public class CalculatorPageObjects {
+public class ElementsPageObjects {
 
-	@FindBy (id = ElementsConfiguration.btn_Calculator)
+	@AndroidFindBy (id = "calculator_button")
 	AndroidElement btnCalculator;
 	
-	@FindBy (id = ElementsConfiguration.btn_Sum)
+	@AndroidFindBy (id = ElementsConfiguration.btn_Sum)
 	AndroidElement btnSum;
 	
-	@FindBy (id = ElementsConfiguration.btn_Div)
+	@AndroidFindBy (id = ElementsConfiguration.btn_Div)
 	AndroidElement btnDiv;
 	
-	@FindBy (id = ElementsConfiguration.btn_Sub)
+	@AndroidFindBy (id = ElementsConfiguration.btn_Sub)
 	AndroidElement btnSub;
 	
-	@FindBy (id = ElementsConfiguration.btn_Mult)
+	@AndroidFindBy (id = ElementsConfiguration.btn_Mult)
 	AndroidElement btnMult;
 	
-	@FindBy (id = ElementsConfiguration.btn_Point)
+	@AndroidFindBy (id = ElementsConfiguration.btn_Point)
 	AndroidElement btnPoint;
 	
-	@FindBy (id = ElementsConfiguration.btnNumber_0)
+	@AndroidFindBy (id = ElementsConfiguration.btnNumber_0)
 	AndroidElement btnZero;
 	
-	@FindBy (id = ElementsConfiguration.btnNumber_1)
+	@AndroidFindBy (id = ElementsConfiguration.btnNumber_1)
 	AndroidElement btnOne;
 	
-	@FindBy (id = ElementsConfiguration.btnNumber_2)
+	@AndroidFindBy (id = ElementsConfiguration.btnNumber_2)
 	AndroidElement btnTwo;
 	
-	@FindBy (id = ElementsConfiguration.btnNumber_3)
+	@AndroidFindBy (id = ElementsConfiguration.btnNumber_3)
 	AndroidElement btnThree;
 	
-	@FindBy (id = ElementsConfiguration.btnNumber_4)
+	@AndroidFindBy (id = ElementsConfiguration.btnNumber_4)
 	AndroidElement btnFour;
 	
-	@FindBy (id = ElementsConfiguration.btnNumber_5)
+	@AndroidFindBy (id = ElementsConfiguration.btnNumber_5)
 	AndroidElement btnFive;
 	
-	@FindBy (id = ElementsConfiguration.btnNumber_6)
+	@AndroidFindBy (id = ElementsConfiguration.btnNumber_6)
 	AndroidElement btnSix;
 	
-	@FindBy (id = ElementsConfiguration.btnNumber_7)
+	@AndroidFindBy (id = ElementsConfiguration.btnNumber_7)
 	AndroidElement btnSeven;
 	
-	@FindBy (id = ElementsConfiguration.btnNumber_8)
+	@AndroidFindBy (id = ElementsConfiguration.btnNumber_8)
 	AndroidElement btnEight;
 	
-	@FindBy (id = ElementsConfiguration.btnNumber_9)
+	@AndroidFindBy (id = ElementsConfiguration.btnNumber_9)
 	AndroidElement btnNine;
 	
 	
-	public CalculatorPageObjects() throws MalformedURLException {
-		PageFactory.initElements(AppiumStart.androidDriver(), this);
-	}
-	
-	
+	public ElementsPageObjects() throws MalformedURLException {
+		PageFactory.initElements(new AppiumFieldDecorator(AppiumStart.androidDriver()), this);
+	}	
 }
