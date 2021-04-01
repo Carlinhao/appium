9c32ee149dc9e681fe57ae1e5b4adddef3a43a9b
Carlinhao <carsilva.ti@gmail.com>
2021-04-01 20:04:30 -0300

    Alterando o tipo do elemento recebido nos métodos da classe

--- src/main/java/appium/utilities/ActionsHelpers.java
+++ src/main/java/appium/utilities/ActionsHelpers.java
@@ -5,28 +5,28 @@ import java.net.MalformedURLException;
 import org.openqa.selenium.interactions.touch.TouchActions;
 
 import appium.base.AppiumStart;
-import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 
 public class ActionsHelpers {
 
-	private AndroidDriver<MobileElement> _driver;
+	private AndroidDriver<AndroidElement> _driver;
 	
 	public ActionsHelpers() throws MalformedURLException {
 		_driver = AppiumStart.androidDriver();
 	}
 	
-	public void tapElement(MobileElement element) {
+	public void tapElement(AndroidElement element) {
 		new TouchActions(_driver)
 		.singleTap(element).perform();
 	}
 	
-	public void doubleTapElement(MobileElement element) {
+	public void doubleTapElement(AndroidElement element) {
 		new TouchActions(_driver)
 		.doubleTap(element).perform();
 	}
 	
-	public void scroolElement(MobileElement element) {
+	public void scroolElement(AndroidElement element) {
 		new TouchActions(_driver)
 		.scroll(element, 10, 100)
 		.perform();;
